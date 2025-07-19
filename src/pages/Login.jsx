5e0590ecfdb43f39e36f0f3ec0d616a52b43836c
RaihanSulthon <raihansy2111@gmail.com>
2025-07-19 14:26:48 +0700

    Fixing/UI/Show Password Tooltip login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -175,6 +175,7 @@ function Login() {
                   type="button"
                   className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700"
                   onClick={() => setShowPassword(!showPassword)}
+                  title=""
                 >
                   {showPassword ? (
                     <svg
@@ -182,6 +183,7 @@ function Login() {
                       fill="none"
                       stroke="currentColor"
                       viewBox="0 0 24 24"
+                      title=""
                     >
                       <path
                         strokeLinecap="round"
